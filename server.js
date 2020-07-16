c6701d8ce3060d2872e8f06c84111a903ff030c3
Madeline Dutro <dutro.madeline@gmail.com>
2020-07-16 00:11:54 -0400

    tooled around with a few different things - just trying to get CRA working in a sensible manner

--- server.js
+++ server.js
@@ -1,5 +1,27 @@
 const express = require('express');
+const session = require('express-session');
+const app = express();
 const {Pool} = require('pg');
+
+const PORT = process.env.PORT || 3333
+
+
+//MIDDLEWARE
+app.set('views', __dirname + '/views');
+
+//CONTROLLERS
+
+//ROUTES
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+//DB CONNECTION
+
+//LISTENER
+app.listen(PORT, () => {
+  console.log("listening on ", PORT);
+})
 
 //Reference the following guide when working with databases
 //https://www.taniarascia.com/node-express-postgresql-heroku/
