32ef675b3a3310b0d9f919d4812ebe997ebdf22a
Madeline Dutro <dutro.madeline@gmail.com>
2020-07-17 01:02:01 -0400

    makes changes to backend - read route working

--- back-end/init.sql
+++ back-end/init.sql
@@ -2,12 +2,15 @@ CREATE TABLE creators(
   ID SERIAL PRIMARY KEY,
   first varchar(180),
   last varchar(180)
-)
+);
 
 CREATE TABLE reviews (
   ID SERIAL PRIMARY KEY,
   site varchar(300),
   reviewtitle varchar(300),
   reviewbody varchar(2000),
-  creatorID INTEGER REFERENCES creators.id
-  )
+  creatorID INTEGER REFERENCES creators (id)
+  );
+
+INSERT INTO creators (first, last) VALUES ('Madeline', 'BigBrainwater');
+INSERT INTO reviews (site, reviewtitle, reviewbody, creatorID) VALUES ('twitter', 'Big Concerns', 'plaintext password leaks and blue checkmark woes abound', 1);
--- back-end/server.js
+++ back-end/server.js
@@ -6,6 +6,7 @@ const {Pool, Client} = require('pg');
 //MIDDLEWARE
 app.set('views', __dirname + '/views');
 require('dotenv').config();
+app.use(express.json());
 
 const PORT = process.env.PORT || 3333
 
@@ -16,6 +17,14 @@ const pool = new Pool({
   password: process.env.DBPASSWORD,
   port: process.env.DBPORT
 })
+
+const client = new Client({
+  database: process.env.DBNAME,
+  user: process.env.DBUSER,
+  password: process.env.DBPASSWORD,
+  port: process.env.DBPORT
+})
+
 pool.connect((err, client, release) => {
   if (err){
     return console.error('Error acquiring client', err.stack)
@@ -31,7 +40,15 @@ pool.connect((err, client, release) => {
 
 //API ROUTES
 
-
+app.get('/', (req, res) => {
+  pool.query('SELECT * FROM reviews', (err, result) => {
+    if (err){
+      console.log(err)
+      throw err
+    }
+    res.json(result.rows);
+  })
+})
 
 //LISTENER
 app.listen(PORT, () => {
